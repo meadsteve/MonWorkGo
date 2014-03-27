cca1d197f3f3c6bfe24cae2e739e789f084f6f93
Steve B <meadsteve@gmail.com>
2014-03-27 20:53:48 +0000

    add examples

---
+++ example-worker.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once __DIR__ . "/vendor/autoload.php";
+
+$mongo = new MongoClient();
+$mongoDB = $mongo->selectDB("test");
+
+class EchoLogger extends \Psr\Log\AbstractLogger
+{
+    public function log($level, $message, array $context = array())
+    {
+        echo $message . PHP_EOL;
+    }
+}
+$logger = new EchoLogger();
+
+$manager = new \MeadSteve\MonWorkGo\Manager($mongoDB, "", $logger);
+
+$manager->createWorker(
+    "mainQueue",
+    function ($payload, \Psr\Log\LoggerInterface $log) {
+        $log->info("Hello. I'm working on: " . var_export($payload, true));
+        return \MeadSteve\MonWorkGo\Worker::WORK_RESPONSE_SUCCESS;
+    }
+)->start();
---
+++ queueing-example.php
@@ -0,0 +1,14 @@
+<?php
+require_once __DIR__ . "/vendor/autoload.php";
+
+$mongo = new MongoClient();
+$mongoDB = $mongo->selectDB("test");
+
+$manager = new \MeadSteve\MonWorkGo\Manager($mongoDB);
+
+$manager->getQueue("mainQueue")
+    ->addWork(["jobOne", 34])
+    ->addWork(["jobTwo", 5, 7])
+    ->addWork(["jobThree", 8])
+    ->addWork(["jobFour", "bob"])
+    ->addWork(["jobFive", "dave"]);
